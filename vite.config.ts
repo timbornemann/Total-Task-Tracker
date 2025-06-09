847accc3464b62e07c1ee311bc28d44236e1f6f1
Tim Bornemann <timbornemann@users.noreply.github.com>
2025-06-09 18:39:12 +0200

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   base: '/',
   server: {
     host: "0.0.0.0",
-    port: 8080,
+    port: 8081,
     proxy: {
       '/api': {
         target: 'http://localhost:3002',
